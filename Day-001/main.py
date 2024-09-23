33bfad5b11dcdd9011eb0f55e9bf433a4c081942
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-23 20:20:55 +0100

    Add a space between the strings.

--- Day-001/main.py
+++ Day-001/main.py
@@ -6,3 +6,6 @@ print("Hello, World!")
 # Ex02: String Manipulation
 # Use '\n' to add another two lines of "Hello, World!"
 print("Hello, World!\nHello, World!\nHello, World!")
+
+# Add a space between the strings
+print("Hello," + " " + "World!")
