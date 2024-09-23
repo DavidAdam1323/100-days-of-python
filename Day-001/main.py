04f214a6aa685e14c261dee040e21b92c1828f7d
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-23 20:48:23 +0100

    Add functionality to check input string length.

--- Day-001/main.py
+++ Day-001/main.py
@@ -15,3 +15,12 @@ print("Hello," + " " + "World!")
 # Use the Python 'input()' function to collect user input and use it within the code
 # e.g. Hello, 'user_input'! 
 print("Hello, " + input("what's your name? ") + "!")
+
+# Ex04: Variables
+# Check the length of the input
+print(len(input("what is your name? ")))
+# Split everything into variables, one variable called username and one called length.
+# Use the variable username in the len calculation.
+user_name = input("What is your name? ")
+length = len(user_name)
+print(f"Your name is {length} characters long!")
