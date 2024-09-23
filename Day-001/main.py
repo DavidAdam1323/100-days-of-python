3d7ac1433764920e9378f07f279b613dce7449ae
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-23 20:17:06 +0100

    Use '\n' to add more lines of 'Hello, World!'

--- Day-001/main.py
+++ Day-001/main.py
@@ -2,3 +2,7 @@
 # Ex01: Printing
 # Print out the words "Hello, World!"
 print("Hello, World!")
+
+# Ex02: String Manipulation
+# Use '\n' to add another two lines of "Hello, World!"
+print("Hello, World!\nHello, World!\nHello, World!")
