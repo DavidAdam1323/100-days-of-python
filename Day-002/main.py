2145b24fac6f9050ab92b5fa7cb072cd9504bcfa
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-24 20:34:16 +0100

    Python primitive data types: Strings, Integers, Floats, and Booleans.

---
+++ Day-002/main.py
@@ -0,0 +1,18 @@
+# 100 Days of Python - Day 002
+
+# Ex01: Python primitive data types.
+# Strings
+# Sequence of characters enclosed in either single quotes ('') or double quotes (“”).
+print(type("123 456"))
+
+# Integers
+# Whole number, positive or negative, without decimals.
+print(type(123_456))
+
+# Floats
+# Fractional numbers, with decimals.
+print(type(123.456))
+
+# Booleans
+# Represent one of two values: True or False.
+print(type(True))
