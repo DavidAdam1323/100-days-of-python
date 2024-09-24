ddf4cf0fbb8fe51a6129dd8982749655dd5c9255
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-24 21:02:02 +0100

    Add example of handling TypeErrors and performing type conversion in Python.

--- Day-002/main.py
+++ Day-002/main.py
@@ -16,3 +16,24 @@ print(type(123.456))
 # Booleans
 # Represent one of two values: True or False.
 print(type(True))
+
+
+# Ex02: Type Error, Checking and Conversion
+# These errors occur when using the wrong data type. 
+#       e.g. len(12345)
+
+# Only give the len() function "Strings", it will give a TypeError if give it a number (Integer).
+# Fix the len() function so it has no more warnings or errors: 
+# print("Number of letters in your name: " + len(input("Enter your name")))
+name = input("Enter your name:\n")
+length = len(name)
+print(f"Your name contains {length} letters!")
+print("Number of letters in your name: " + str(length))
+print(str(len(input("Enter your name: "))))
+
+# Type Conversion
+# Convert data into different data types using special functions. 
+#   e.g.
+#       float()
+#       int()
+#       str()
