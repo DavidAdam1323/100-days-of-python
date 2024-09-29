05b83f1b75ce33d51f71949e91347efa42062285
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-29 21:48:07 +0100

    Started building quiz game with JavaScript-themed questions and game over logic.

--- Day-003/main1.py
+++ Day-003/main1.py
@@ -1,3 +1,45 @@
+# 100 Days of Python - Day 003
 
+# Ex07: Game Project
+def game_over(reason):
+    print("""
+     ____                         ___                
+    / ___| __ _ _ __ ___   ___   / _ \__   _____ _ __ 
+   | |  _ / _` | '_ ` _ \ / _ \ | | | \ \ / / _ \ '__|
+   | |_| | (_| | | | | | |  __/ | |_| |\ V /  __/ |   
+    \____|\__,_|_| |_| |_|\___|  \___/  \_/ \___|_|   
+    """)
+    print(f"{reason} Game Over!")
+    exit()
+
+def get_valid_input(prompt, options):
+  while True:
+    choice = input(prompt).lower()
+    if choice in options:
+      return choice
+    print(f"Invalid choice. Please choose from {', '.join(options)}.")
+
+def javascript_quiz():
+  print("Welcome to JavaScript Quiz.")
+  print("Answer the question correctly to pass the quiz. Let's begin!")
+
+  # level 1:
+  question1 = """
+  Which of the following is the correct way to declare a variable in JavaScript?
+    a) let variableName;
+    b) var variableName;
+    c) const variableName = value;
+    d) All of the above
+
+  To enter your answer, please type a, b, c, or d. 
+  """
+  
+  answer1 = get_valid_input(question1, ["a", "b", "c", "d"])
+  if answer1.lower() != "d":
+        game_over("I'm sorry, wrong answer - better luck next time.")
+  else:
+     print("Congratulations! You passed the quiz and have a solid grasp of JavaScript basics!")
+    
+javascript_quiz()
 
 
