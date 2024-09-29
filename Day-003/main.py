a325441db687133b3a30eb74a4240fc15c976d56
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-29 17:22:33 +0100

    Learned conditionals and operators. Built height check program for rollercoaster.

---
+++ Day-003/main.py
@@ -0,0 +1,37 @@
+# 100 Days of Python - Day 003
+
+# Conditional Statements, Logical Operators, Code Blocks and Scope
+# Ex01: "If" / "Else" - Conditional Check
+# Conditionals in Python tells the computer what to do in each case. e.g.
+
+# Condition Check:
+# if <this condition is true>:
+#     <then execute this line of code>
+
+# What if the condition is false?
+# The "else" keyword defines a block of code that will execute, 
+# if the condition checked in the "if" statement is false.
+
+# if pigs can fly:
+#     <Some code that will never execute>
+# else:
+#     print("This is real life")
+
+print("Welcome to the Rollercoaster!")
+height = input("What is your height in cm?\n")
+if height >= "120":
+  print("Yes! You can ride.")
+else:
+  print("No. You can't ride.")
+
+# Ps: Indentation is important in Python because it 
+# helps make code more readable and maintainable, and
+# it indicates the start and end of code blocks.
+
+# Comparator Operators:
+#   > Greater than
+#   < Less than
+#   >= Greater than or equal to
+#   <= Less than or equal to
+#   == Equal to
+#   != Not equal to
