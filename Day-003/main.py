71c56c2bf3ecc6d54d3adc727803350c7b12b4db
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-29 19:59:15 +0100

    Learn and add  pizza order program with size, toppings, and final bill calculation.

--- Day-003/main.py
+++ Day-003/main.py
@@ -129,3 +129,46 @@ if height >= 120:
 
 else:
   print("Sorry, you have to grow taller before you can ride.")
+
+
+# Ex05:
+# Based on a user's order, work out their final bill. 
+# Use the input() function to get a user's preferences 
+# and then add up the total for their order and tell them how much they have to pay.
+
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +£2
+# Add pepperoni for medium or large pizza (Y or N): +£3
+# Add extra cheese for any size pizza (Y or N): +£1
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want?\nType S, M or L: ").upper()
+pepperoni = input('Would you like to add extra Pepperoni?\nType "Yes" or "No": ').lower()
+extra_cheese = input('Would you like to add extra Cheese?\nType "Yes" or "No": ').lower()
+
+S = 15
+M = 20
+L = 25
+
+bill = 0
+
+if size == "S":
+  bill += S
+elif size == "M":
+  bill += M
+elif size == "L":
+  bill += L
+else:
+  print("Enter a valid input please!")
+
+if pepperoni == "yes":
+  if size == "S":
+    bill += 2
+  else:
+    bill += 3
+
+if extra_cheese == "yes":
+  bill += 1
+
+print(f"Your final bill is £{bill}.") 
