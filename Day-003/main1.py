b7f0d5cae37da92e9c70f42d2e860b23ccbcb174
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-29 22:17:55 +0100

    Added final question on array creation in JavaScript.

--- Day-003/main1.py
+++ Day-003/main1.py
@@ -21,7 +21,7 @@ def get_valid_input(prompt, options):
 
 def javascript_quiz():
   print("Welcome to JavaScript Quiz.")
-  print("Answer the question correctly to pass the quiz. Let's begin!")
+  print("Answer the questions correctly to move to the next level. Let's begin!")
 
   # level 1:
   question1 = """
@@ -35,7 +35,7 @@ def javascript_quiz():
   """
   answer1 = get_valid_input(question1, ["a", "b", "c", "d"])
   if answer1.lower() != "d":
-      game_over("I'm sorry, wrong answer - better luck next time.")
+    game_over("I'm sorry, wrong answer - better luck next time.")
 
   # level 2:
   question2 = """
@@ -49,7 +49,7 @@ def javascript_quiz():
   """
   answer2 = get_valid_input(question2, ["a", "b", "c", "d"])
   if answer2.lower() != "d":
-      game_over("I'm sorry, wrong answer - better luck next time.")
+    game_over("I'm sorry, wrong answer - better luck next time.")
 
   # level 3:
   question3 = """
@@ -64,10 +64,23 @@ def javascript_quiz():
   """
   answer3 = get_valid_input(question3, ["a", "b", "c", "d"])
   if answer3.lower() != "b":
-      game_over("I'm sorry, wrong answer - better luck next time.")
+    game_over("I'm sorry, wrong answer - better luck next time.")
 
+  # level 4:
+  question4 = """
+  Which of the following is the correct way to create an array in JavaScript?
+    a) let myArray = [1, 2, 3];
+    b) let myArray = (1, 2, 3);
+    c) let myArray = {1, 2, 3};
+    d) let myArray = 1, 2, 3;
+
+  To enter your answer, please type a, b, c, or d.
+  """
+  answer4 = get_valid_input(question4, ["a", "b", "c", "d"])
+  if answer4.lower() == "a":
+    print("Congratulations! You passed the quiz and have a solid grasp of JavaScript basics!")
   else:
-     print("Congratulations! You passed the quiz and have a solid grasp of JavaScript basics!")
+    game_over("I'm sorry, wrong answer - better luck next time.")
     
 javascript_quiz()
 
