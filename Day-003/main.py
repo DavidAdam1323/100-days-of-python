e87d79a9ba3369c3947665c19f4c617b8dd7741f
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-29 18:20:12 +0100

    Learn modulo operator example and odd/even number checker.

--- Day-003/main.py
+++ Day-003/main.py
@@ -1,6 +1,6 @@
 # 100 Days of Python - Day 003
-
 # Conditional Statements, Logical Operators, Code Blocks and Scope
+
 # Ex01: "If" / "Else" - Conditional Check
 # Conditionals in Python tells the computer what to do in each case. e.g.
 
@@ -35,3 +35,19 @@ else:
 #   <= Less than or equal to
 #   == Equal to
 #   != Not equal to
+
+# Ex02: Modulo
+# The modulo operator gives you the remainder of a division.
+print(10 % 3)
+
+# 6 % 2 #will be 0
+# 6 % 5 #will be 1
+# 6 % 4 #will be 2
+
+# Check Odd or Even:
+print("Hello, let's find out if it's an Odd or Even number!")
+num = int(input("Please enter a number:\n"))
+if num % 2 == 0:
+  print(f"The number {num} is an Even number.")
+else:
+  print(f"The number {num} is an Odd number.")
