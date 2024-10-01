1d2347e425d9ce397fdef26ef39d12f80e78643f
DavidAdam1323 <davidadamgtr@gmail.com>
2024-10-01 20:04:28 +0100

    Added examples of For Loops with range() and step size, including Gauss challenge for sum of numbers 1-100.

--- Day-005/main.py
+++ Day-005/main.py
@@ -36,3 +36,36 @@ for score in student_scores:
       highest_score = score
 
 print(highest_score)
+
+# Ex03: For Loops with Range
+# Range function "range()": range(1, 10)
+# This code doesn't do anything by itself. 
+# For example, if you tried to print it, it would not give you individual numbers.
+# But it can be used in conjunction with For Loops.
+for number in range(1, 10):
+    print(number)
+# This will print out each of the numbers 1 - 9. 
+# So the range can also be expressed like this: "a <= range(a, b) < b"
+
+# In the code:
+# The `range()` function generates a sequence of numbers based on three parameters: `start`, `stop`, and `step`.
+for num in range(1, 11, 3): # The `3` dictates how much the `num` increases after each iteration (the step size).
+    print(num)
+#     a. `1` is the starting value (inclusive). The loop starts at 1.
+#     b. `11` is the stopping value (exclusive). The loop will stop before reaching 11.
+#     c. `3` is the step value. This means the loop will increment the variable `num` by 3 after each iteration.
+
+# The loop will generate numbers starting from 1, and with each iteration, it will increase by 3. 
+# The numbers printed will be:
+#                             First iteration: `1`
+#                             Second iteration: `4`
+#                             Third iteration: `7`
+#                             Fourth iteration: `10`
+
+# The Gauss Challenge
+# Work out the total of the numbers between 1 and 100, inclusive of both 1 and 100.
+total = 0
+for number in range(1, 101):
+   total += number
+
+print(total)
