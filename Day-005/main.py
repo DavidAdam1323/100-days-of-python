3bcc95079fa30a09bb442181e609c406ad0329cc
DavidAdam1323 <davidadamgtr@gmail.com>
2024-10-01 19:28:42 +0100

    Add Ex02: Highest Score Challenge - Use loops and conditionals to find the highest score in a list.

--- Day-005/main.py
+++ Day-005/main.py
@@ -24,3 +24,15 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
 print("Hello")
+
+# Ex02: Challenge - Print the highest score of the list.
+# Use For Loops and Conditionals to print out the highest score in the list of student_scores. 
+# For example, if the scores were: 8 65 89 86 55 91 64 89
+# Your code should print 91.
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+highest_score = 0
+for score in student_scores:
+   if score > highest_score:
+      highest_score = score
+
+print(highest_score)
